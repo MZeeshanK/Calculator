e0ca2cfcb6be9b2deec8a520d9c9c0335bf05976
MZeeshanK <93533777+MZeeshanK@users.noreply.github.com>
2022-05-14 00:29:44 +0530

    Second Commit

--- app.js
+++ app.js
@@ -6,26 +6,8 @@ operations = document.querySelectorAll('.btn-operation'),
 numbers = document.querySelectorAll('.btn-number'),
 equals = document.querySelector('#equals');
 
-allClear.addEventListener('click',clear)
 
-numbers.forEach((number)=>{
-  number.addEventListener('click',()=> {
-    if(number.classList.contains('btn-small') && value.value.includes('.')){
-      value.value +='';
-    }else{
-      value.value += number.textContent
-    }
-  })
-})
-
-operations.forEach((operation)=>{
-  operation.addEventListener('click',()=>{
-    value.value += operation.textContent;
-    preValue.value = value.value;
-    value.value='';
-    calculate();
-  })
-})
+allClear.addEventListener('click',clear);
 
 del.addEventListener('click', ()=> {
   let str = value.value;
@@ -33,38 +15,107 @@ del.addEventListener('click', ()=> {
   value.value = str;
 });
 
+numbers.forEach((number)=>{
+  number.addEventListener('click',()=> {
+    let str = value.value;
+    if(number.classList.contains('btn-small') && str.includes('.')){
+      str +='';
+    }
+    else{
+      str += number.textContent;
+    }
+    value.value = str;
+    operations.forEach((operation)=>{
+      if(str.includes(operation.textContent)){
+       str = str.substring(0,str.indexOf(operation.textContent))
+       preValue.value = str;
+      }
+    })
+  })
+})
+
+operations.forEach((operation) => {
+  operation.addEventListener('click',()=>{
+    let str = value.value;
+    if(str === ''){
+      str = '';
+    }else if(str.slice(-1) === '+' || str.slice(-1) === '-' || str.slice(-1) === '*' || str.slice(-1) === '/' || str.slice(-1) === '%'){
+        switch(operation.textContent){
+          case '+':
+            str = str.substring(0,str.length -1) + '+';
+            break;
+          case '-':
+            str = str.substring(0,str.length -1) + '-';
+            break;
+          case '*':
+            str = str.substring(0,str.length -1) + '*';
+            break;
+          case '/':
+            str = str.substring(0,str.length -1) + '/';
+            break;
+          case '%':
+            str = str.substring(0,str.length -1) + '%';
+            break;
+          default:
+            return;
+      }
+    }
+    else{
+      str += operation.textContent;
+    }
+    value.value = str;
+  })
+})
+
 equals.addEventListener('click',calculate)
 
 function calculate(){
-  let cal;
-  const a = parseFloat(preValue.value),
-  b = parseFloat(value.value);
-  if(isNaN(a) || isNaN(b)){
-    return ;
-  }
+  let cal,
+  str= value.value;
+  let i = [];
+  operations.forEach((operation) => {
+    for(x=0;x<str.length;x++){
+      if(str[x]===operation.textContent){
+        i.push(x);
+      }
+    }
+  })
+  const y = i[i.length-1],
+  z = i[0];
+  let a;
+  let b;
+  b = parseFloat(str.substring(y+1,str.length));
+  a = parseFloat(str.substring(0,z));
 
-  switch(preValue.value.substring(preValue.value.length-1))
-  {
+  switch(str[z] || str[y]){
     case '+':
-      cal = a + b;
+      cal = a+b;
       break;
     case '-':
-      cal = a - b;
+      cal = a-b;
       break;
     case '*':
-      cal = a * b;
+      cal = a*b;
       break;
     case '/':
-      cal = a / b;
+      cal = a/b;
       break;
     case '%':
       cal = (a/b)*100;
       break;
     default:
-      return ;
-    }
-  value.value = cal;
+      return;
+  }
+
+  preValue.value = cal;
+
+  if(i.length>1){
+    a = parseFloat(str.preValue);
+    preValue.value = cal;
+  }
 }
+
+
 
 function clear(){
   value.value = '';
